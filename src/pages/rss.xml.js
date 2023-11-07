a749533b6ad8c1e422515e9169d8ad6e83da543c
Joseph Trubenstein <josephtrubenstein@gmail.com>
2023-11-07 09:18:59 +0900

    rss feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,16 @@
 import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
+import { getAllPosts } from "../utils/api";
 import config from '../config/config.json';
 
 export async function GET(context) {
-  const posts = await getCollection('blog');
+  const posts = await getAllPosts();
   return rss({
     title: config.site.title,
     description: config.site.description,
     site: context.site,
     items: posts.map((post) => ({
       ...post.data,
-      link: `/blog/${post.slug}/`,
+      link: `/${post.slug}/`,
     })),
   });
 }
